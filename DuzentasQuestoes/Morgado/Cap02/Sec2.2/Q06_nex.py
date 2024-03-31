ce3d1c2262a746fe8558dc7d9c55758041c9f65e
Thales F <thalesfelix0025@gmail.com>
2024-03-31 00:43:00 -0300

    Adicionando exercícios na  Secção 2.2

---
+++ DuzentasQuestoes/Morgado/Cap02/Sec2.2/Q06_nex.py
@@ -0,0 +1,5 @@
+import permutar as p
+
+res = p.permutacao(10) // 6
+
+print(res, "\n")
---
+++ DuzentasQuestoes/Morgado/Cap02/Sec2.2/Q07_nex.py
@@ -0,0 +1,32 @@
+import permutar as p
+
+
+print("Item a)\n")
+
+res = p.permutacao(12) // (p.permutacao(6) * p.permutacao(6) * 2)
+
+print(res, "\n")
+
+print("Item b)\n")
+
+res = p.permutacao(12) // (p.permutacao(4) * p.permutacao(4) * p.permutacao(4) * p.permutacao(3))
+
+print(res, "\n")
+
+print("Item c)\n")
+
+res = p.permutacao(12) // (p.permutacao(5) * p.permutacao(7))
+
+print(res, "\n")
+
+print("Item d)\n")
+
+res = p.permutacao(12) // (p.permutacao(2) * p.permutacao(2) * p.permutacao(2) * p.permutacao(2) * p.permutacao(2) * p.permutacao(2) * p.permutacao(6)  )
+
+print(res, "\n")
+
+print("Item e)\n")
+
+res = p.permutacao(12) // (p.permutacao(2) * p.permutacao(2) * p.permutacao(4)**2 * 2**2)
+
+print(res, "\n")
