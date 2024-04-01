56cf6be179bd5a296492a17eea3e9608753ff86a
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2024-04-01 15:33:52 -0300

    Delete DuzentasQuestoes/Morgado/Cap02/Sec2.2 directory
    Reorganização da estrutura do projeto.

--- DuzentasQuestoes/Morgado/Cap02/Sec2.2/Q10.py
+++
@@ -1,5 +0,0 @@
-import permutar as p
-
-r = p.permutacao(6) # Possibilidades de faces do dado já que cada face é diferenciável por apresentar cores diferentes
-
-print(r, "\n") 
--- DuzentasQuestoes/Morgado/Cap02/Sec2.2/permutar.py
+++
@@ -1,5 +0,0 @@
-def permutacao(n):
-  if(n == 0):
-    return 1
-  else:
-    return n * permutacao(n - 1)
